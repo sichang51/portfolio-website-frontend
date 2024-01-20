0442f3a713863f7099aeac5abe01b43ae331ad91
sichang51 <99237899+sichang51@users.noreply.github.com>
2024-01-20 09:48:53 -0800

    formatted h1 and h2 text, formatted social media icons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,9 +10,9 @@ const Main = () => {
         src="https://www.visittheusa.com/sites/default/files/styles/hero_l/public/images/hero_media_image/2017-04/245d5f259ae4a3de3e9a4d4e062cb555.jpeg?h=9c2070a3&itok=YnwWoUKQ"
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
-        <div>
-          <h1>I'm Simon Chang</h1>
-          <h2>
+        <div className="max-w-[700px] m-auto, h-full w-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">I'm Simon Chang</h1>
+          <h2 className="flex sm:text-3xl text2xl pt-4 text-gray-800">
             I'm a
             <TypeAnimation
               sequence={[
@@ -31,11 +31,11 @@ const Main = () => {
               repeat={Infinity}
             />
           </h2>
-          <div>
-            <FaTwitter />
-            <FaFacebookF />
-            <FaInstagram />
-            <FaLinkedinIn />
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <FaTwitter className="cursor-pointer size={20}" />
+            <FaFacebookF className="cursor-pointer size={20}" />
+            <FaInstagram className="cursor-pointer size={20}" />
+            <FaLinkedinIn className="cursor-pointer size={20}" />
           </div>
         </div>
       </div>
