fdc8488e0c150ceb25fb75b866b2ca7fb4490605
sichang51 <99237899+sichang51@users.noreply.github.com>
2024-01-20 10:15:34 -0800

    created Work.jsx and WorkItem.jsx in components folder, added fake data and import WorkItem to Work.jsx, add structure and formatting to WorkItem.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import WorkItem from "./Workitem";
 
+const data = [
+  {
+    year: 2020,
+    title: "Content Creator",
+    duration: "3 Years",
+    details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur omnis dolor numquam, perspiciatis illo eius recusandae eum, expedita placeat asperiores",
+  },
+  {
+    year: 2017,
+    title: "Google",
+    duration: "3 Years",
+    details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur omnis dolor numquam, perspiciatis illo eius recusandae eum, expedita placeat asperiores",
+  },
+  {
+    year: 2015,
+    title: "Amazon",
+    duration: "3 Years",
+    details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur omnis dolor numquam, perspiciatis illo eius recusandae eum, expedita placeat asperiores",
+  },
+  {
+    year: 2012,
+    title: "Facebook",
+    duration: "3 Years",
+    details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur omnis dolor numquam, perspiciatis illo eius recusandae eum, expedita placeat asperiores",
+  },
+];
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div id="work" className="max-w[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Work</h1>
+      {data.map((item, idx) => (
+        <WorkItem key={idx} year={item.year} title={item.title} duration={item.duration} details={item.details} />
+      ))}
+    </div>
+  );
 };
 
 export default Work;
