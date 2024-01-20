d7e806d36b4630725b105a282b24ee25300dadf9
sichang51 <99237899+sichang51@users.noreply.github.com>
2024-01-20 09:00:15 -0800

    added classNames and elements to Sidenav.jsx, disabled css.validate in VSCode settings and installed Tailwind CSS IntelliSense to VSCode

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineHome } from "react-icons/ai";
 
 const Sidenav = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+    console.log("state changed");
+  };
+
   return (
     <div>
-      <AiOutlineMenu />
+      <AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden" />
+      {nav ? (
+        <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+          <a href="#main" className="w-[75%] flex justify-center">
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Home</span>
+          </a>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
