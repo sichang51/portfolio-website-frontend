0b043504e6a215cadb257fef5067b13473c7fea6
sichang51 <99237899+sichang51@users.noreply.github.com>
2024-01-25 19:23:06 -0800

    adjusted CSS to center Title and More Info button in ProjectItem.jsx

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const ProjectItem = ({ img, title }) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+    <div className="relative group flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
       <img src={img} alt="/" className="rounded-xl group-hover:opacity-10" />
-      <div className="hidden group-hover:block absolute top-[25%] left-[10%] translate-x-[50%] translate-y-[5%]">
-        <h3 className="text-2xl font-bold text-white tracking-wider text-center">{title}</h3>
-        <p className="pb-4 pt-2 text-white text-center">React Js</p>
+      <div className="hidden group-hover:flex flex-col items-center justify-center absolute inset-0">
+        <h3 className="text-2xl font-bold text-white tracking-wider mb-2">{title}</h3>
+        <p className="text-white mb-4">React Js</p>
         <a href="/">
           <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">
             More Info
