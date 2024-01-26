f7aa527379e98f0370135b9cdd7a13b3ad013b09
sichang51 <99237899+sichang51@users.noreply.github.com>
2024-01-25 20:11:52 -0800

    attempted to show text when hovering over sidenav buttons to fail

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -5,6 +5,7 @@ import { GrProjects } from "react-icons/gr";
 
 const Sidenav = () => {
   const [nav, setNav] = useState(false);
+
   const handleNav = () => {
     setNav(!nav);
   };
@@ -25,6 +26,7 @@ const Sidenav = () => {
           <a
             onClick={handleNav}
             href="#work"
+            title="Work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
