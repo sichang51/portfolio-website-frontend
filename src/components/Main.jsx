a7b539ac739f6f210bc67042474a00371ce0fd4f
sichang51 <99237899+sichang51@users.noreply.github.com>
2024-01-25 19:38:37 -0800

    fixed alignment H1 and H2 text on Main.jsx by removing commas after H2 wrapper

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,13 +4,14 @@ import { FaTwitter, FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/f
 
 const Main = () => {
   return (
-    <div id="main">
+    <div id="main" className="relative">
       <img
-        className="w-full h-screen object-cover object-left"
+        className="w-full h-screen object-cover object-left scale-x-[-1]"
         src="https://www.visittheusa.com/sites/default/files/styles/hero_l/public/images/hero_media_image/2017-04/245d5f259ae4a3de3e9a4d4e062cb555.jpeg?h=9c2070a3&itok=YnwWoUKQ"
+        alt="/"
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
-        <div className="max-w-[700px] m-auto, h-full w-full flex flex-col justify-center lg:items-start items-center">
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
           <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">I'm Simon Chang</h1>
           <h2 className="flex sm:text-3xl text2xl pt-4 text-gray-800">
             I'm a
