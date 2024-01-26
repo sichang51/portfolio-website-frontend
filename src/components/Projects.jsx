e7e8f016fdd99b787c3ed614ead87a35bedca372
sichang51 <99237899+sichang51@users.noreply.github.com>
2024-01-25 19:30:37 -0800

    fixed Projects.jsx so that navbar moves to project list when clicked

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ import placeHolderImg from "../assets/placeholder.jpg";
 
 export const Projects = () => {
   return (
-    <div id="project" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
       <p className="text-center py-8">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
