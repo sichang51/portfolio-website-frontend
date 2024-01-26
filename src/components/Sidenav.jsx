661e7d6976ce0e6d26e21bcb93a4701f12ccf2a6
sichang51 <99237899+sichang51@users.noreply.github.com>
2024-01-25 20:13:55 -0800

    removed title from Work in Sidenav.jsx

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -26,7 +26,6 @@ const Sidenav = () => {
           <a
             onClick={handleNav}
             href="#work"
-            title="Work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
