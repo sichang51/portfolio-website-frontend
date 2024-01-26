205aa4e71feaace659404a377deedcd5d1bda597
sichang51 <99237899+sichang51@users.noreply.github.com>
2024-01-25 19:10:03 -0800

    added onClick={handleNav} to all navBar elements

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -15,6 +15,7 @@ const Sidenav = () => {
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
           <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -22,6 +23,7 @@ const Sidenav = () => {
             <span className="pl-4">Home</span>
           </a>
           <a
+            onClick={handleNav}
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -29,6 +31,7 @@ const Sidenav = () => {
             <span className="pl-4">Work</span>
           </a>
           <a
+            onClick={handleNav}
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -36,6 +39,7 @@ const Sidenav = () => {
             <span className="pl-4">Projects</span>
           </a>
           <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -43,6 +47,7 @@ const Sidenav = () => {
             <span className="pl-4">Resume</span>
           </a>
           <a
+            onClick={handleNav}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
